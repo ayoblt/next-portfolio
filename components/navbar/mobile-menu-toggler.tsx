78f4b1148b0224cb0c002a5a2cac1c249e887035
Lateef Taiwo <ayomidepakoo@gmail.com>
2024-03-13 20:08:38 +0100

    change padding to width and height for menu-toggler

--- components/navbar/mobile-menu-toggler.tsx
+++ components/navbar/mobile-menu-toggler.tsx
@@ -10,7 +10,7 @@ const MobileMenuToggler = () => {
     <div
       onClick={handleToggle}
       className={`fixed md:hidden antialiased backdrop-blur-xl flex items-center justify-center bg-primary dark:bg-backgroundDarker hover:bg-backgroundDarker/60 shadow-md dark:shadow-[3px_0px_150px_2px] shadow-[#011627]/30 rounded-full cursor-pointer bottom-7 left-1/2 -translate-x-1/2 z-50 w-fit transition-all duration-300
-        ${isOpen ? "gap-0 h-10 w-10" : "gap-2 h-10 px-5 w-auto"}
+        ${isOpen ? "gap-0 h-10 w-10" : "gap-2 h-10 w-24"}
       `}
     >
       <div className="flex flex-col items-center justify-center gap-[3px] transition-all duration-300">
