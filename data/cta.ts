d7e6da34d715f96b61a295f163fa8cb64c8e465b
Lateef Taiwo <ayomidepakoo@gmail.com>
2024-03-13 19:03:02 +0100

    removed unused data files

--- data/cta.ts
+++
@@ -1,6 +0,0 @@
-export const CTAData = {
-  title: "Let's collaborate",
-  description: `Ready to start your web project or have questions? I'm just a
-  message away. Let's discuss your ideas and bring them to life.`,
-  path: "/contact-me",
-};
