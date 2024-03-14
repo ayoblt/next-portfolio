3a233c8f9e7addfe6072098a028739b362090409
Lateef Taiwo <ayomidepakoo@gmail.com>
2024-03-14 22:32:17 +0100

    Added metadata for home and about page

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -2,6 +2,13 @@ import CTA from "@/components/cta";
 import Header from "./header";
 import WhyChooseMe from "./why-choose-me";
 import MyFunFacts from "./my-fun-facts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About | Lateef Taiwo",
+  description:
+    "Building responsive and performant websites - Lateef's expertise in Front-End Development.",
+};
 
 export default function About() {
   return (
